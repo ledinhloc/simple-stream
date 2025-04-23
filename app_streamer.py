9cfc08f2d762e0a868102398a11895335fdec35a
ledinhloc <ledinhloc7@gmail.com>
2025-04-23 09:06:02 +0700

    update

--- app_streamer.py
+++ app_streamer.py
@@ -22,14 +22,31 @@ with col1:
     ctx = webrtc_streamer(key="example", 
                           video_frame_callback=video_frame_callback,
 
-                          rtc_configuration={  # Add this config
-                            "iceServers": [
-                                {"urls": ["turn:openrelay.metered.ca:443"],
-                                 "username": "openrelayproject",
-                                 "credential": "openrelayproject"
-                                 }
+                        #   rtc_configuration={  # Add this config
+                        #     "iceServers": [
+                        #         {"urls": ["turn:openrelay.metered.ca:443"],
+                        #          "username": "openrelayproject",
+                        #          "credential": "openrelayproject"
+                        #          }
+                        #         ]
+                        #     },
+                        rtc_configuration={
+                                'iceServers': [
+                                    {
+                                    'url': 'stun:stun.l.google.com:19302'
+                                    },
+                                    {
+                                    'url': 'turn:192.158.29.39:3478?transport=udp',
+                                    'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                                    'username': '28224511:1379330808'
+                                    },
+                                    {
+                                    'url': 'turn:192.158.29.39:3478?transport=tcp',
+                                    'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                                    'username': '28224511:1379330808'
+                                    }
                                 ]
-                            },
+                                },
                           media_stream_constraints={"video": True, "audio": False})
 
 imgout_place = col2.empty()
