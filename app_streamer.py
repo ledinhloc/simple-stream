5d4171e62d9f23870b34731b822b51c44cc33ef2
ledinhloc <ledinhloc7@gmail.com>
2025-04-23 08:58:50 +0700

    upadte

--- app_streamer.py
+++ app_streamer.py
@@ -24,11 +24,9 @@ with col1:
 
                           rtc_configuration={  # Add this config
                             "iceServers": [
-                                {"urls": ["stun:stun.l.google.com:19302"],
-                                 "urls": ["stun:stun1.l.google.com:19302"],
-                                 "urls": ["stun:stun2.l.google.com:19302"],
-                                 "urls": ["stun:stun3.l.google.com:19302"],
-                                 "urls": ["stun:stun4.l.google.com:19302"],
+                                {"urls": ["turn:openrelay.metered.ca:443"],
+                                 "username": "openrelayproject",
+                                 "credential": "openrelayproject"
                                  }
                                 ]
                             },
